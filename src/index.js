c03dde329ca259076f40b55ca916b011e7a02979
EvelinSA05 <evelinsalsabila27@gmail.com>
2024-12-25 08:12:42 +0700

    portfolio

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <BrowserRouter basename="/tugas-XII-sem5/portfolio-website-react"> {/* Wrap dengan BrowserRouter */}
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
